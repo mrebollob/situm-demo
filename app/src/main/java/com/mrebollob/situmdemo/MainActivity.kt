f6191752d358aa8562f417e34706225647c76a75
Manuel Rebollo <mrebollob@gmail.com>
2017-06-14 13:45:53 +0200

    Added situm info buttons

--- app/src/main/java/com/mrebollob/situmdemo/MainActivity.kt
+++ app/src/main/java/com/mrebollob/situmdemo/MainActivity.kt
@@ -1,5 +1,8 @@
 package com.mrebollob.situmdemo
 
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.widget.Button
@@ -9,6 +12,8 @@ import com.mrebollob.situmdemo.utils.bindView
 class MainActivity : AppCompatActivity() {
 
     val mapButton: Button by bindView(R.id.map_button)
+    val mappingAppButton: Button by bindView(R.id.mapping_app_button)
+    val situmLinkButton: Button by bindView(R.id.situm_link_button)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,6 +21,30 @@ class MainActivity : AppCompatActivity() {
 
         mapButton.setOnClickListener {
             MapsActivity.open(this)
+        }
+
+        mappingAppButton.setOnClickListener {
+            openMappingApp()
+        }
+
+        situmLinkButton.setOnClickListener {
+            val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse("https://situm.es/en"))
+            startActivity(browserIntent)
+        }
+    }
+
+    private fun openMappingApp() {
+        val appPackageName = "es.situm.maps"
+
+        try {
+            val LaunchIntent = packageManager.getLaunchIntentForPackage(appPackageName)
+            startActivity(LaunchIntent)
+        } catch (anfe: ActivityNotFoundException) {
+            try {
+                startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=" + appPackageName)))
+            } catch (anfe: ActivityNotFoundException) {
+                startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=" + appPackageName)))
+            }
         }
     }
 }
